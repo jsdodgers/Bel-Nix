6d0addf8e20dad9b212374ac21001ca6eeb1e1a9
Ryan Torres <ryanmt@uci.edu>
2014-11-16 00:12:05 -0800

    Adding weapon functionality.

---
+++ BelNix/Assets/Scripts/Item1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ItemType {WEAPON, ARMOR, USEABLE, AMMUNITION, MISC}
+
+public class Item1 : MonoBehaviour {
+
+	public string itemName;
+	public ItemType itemType;
+	public int gold, silver, copper;
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
---
+++ BelNix/Assets/Scripts/ItemWeapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum DamageType {CRUSHING, PIERCING, SLASHING}
+
+[RequireComponent (typeof (Character))]
+public class ItemWeapon : MonoBehaviour {
+	public int hit;
+	public int range;
+	public int numberOfDamageDice;
+	public int diceType;
+	public int damageBonus;
+	public DamageType damageType;
+	public int criticalChance;
+	public int durabilityChance;
+}
