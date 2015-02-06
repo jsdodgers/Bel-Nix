0b0ec1b0cde4c213c145ead965f13259b6549ac1
Connor Richards <connorar@uci.edu>
2015-02-05 21:19:38 -0800

    Added a bunch of blood stuff, and am currently testing to make a good way to make it appear.

---
+++ BelNix/Assets/BloodTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodTest : MonoBehaviour {
+
+	public GameObject blood;
+	public GameObject blood1;
+	public GameObject blood2;
+	public string bloodstring;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			GameObject.Instantiate(blood1);
+			//transform.localScale.Set(4.0f, 4.0f, 0.0f);
+
+		}
+	}
+}
