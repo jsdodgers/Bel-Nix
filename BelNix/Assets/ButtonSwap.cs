9d708a4417f97cc58b49f166fbb6fafdec18cc7e
jesteven <jesteven@uci.edu>
2015-02-08 21:56:15 -0800

    Added polished Restore/Minimize buttons for console and action buttons and hooked up the logic for them. Changed a few animations

---
+++ BelNix/Assets/ButtonSwap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonSwap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //GetComponent<Button>().image.sprite = defaultSprite;
+        //currentSprite = defaultSprite;
+	}
+
+    public void toggleSprite()
+    {
+        GetComponent<Animator>().SetBool("CurrentlyDefault", !GetComponent<Animator>().GetBool("CurrentlyDefault"));
+    }
+}
