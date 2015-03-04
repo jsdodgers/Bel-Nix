99c3df9f95d282cc3ffd320e5fd089480d1f64c5
Jsdodgers <jsdodgers@aim.com>
2015-03-03 20:47:24 -0800

    Fixed move and loot.

--- BelNix/Assets/Scripts/RightClickButton.cs
+++ BelNix/Assets/Scripts/RightClickButton.cs
@@ -123,9 +123,7 @@ public class RightClickButton : MonoBehaviour {
 
 		}*/
 		if (action.minorTypes.Contains(MinorType.Loot)) {
-			if (!UnitGUI.inventoryOpen) UnitGUI.clickTab(Tab.B);
-			BattleGUI.clearLootItems();
-			BattleGUI.setLootItems(action.actualTile.getItems(), action.actualTile);
+			u.lootTile = action.actualTile;
 		}
 		RightClickMenu.hideMenu(true);
 	}
