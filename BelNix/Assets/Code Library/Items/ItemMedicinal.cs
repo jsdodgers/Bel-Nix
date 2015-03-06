f1b2564a19ba0378f44550cb56e929b5ffe49f08
Ryan Torres <ryanmt@uci.edu>
2015-03-05 21:13:45 -0800

    Did a bunch of stuff with inventory icons.

---
+++ BelNix/Assets/Code Library/Items/ItemMedicinal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemMedicinal : ItemWeapon {
+
+	public override Item getItem() {
+		return getMedKit();
+	}
+	
+	public virtual Medicinal getMedKit() {
+		//		if (weapon == null)
+		/*	string s = AssetDatabase.GetAssetPath(spritePrefab);
+		if (s != null && s.Length >= 17) {
+			s = s.Substring(17, s.Length - 17 - 7);
+		}
+		Debug.Log("Weapon Asset Path: " + s);*/
+		//	if (s != null && s != "")
+		//		GameObject.Instantiate(Resources.Load<GameObject>(s));
+		return new Medicinal(itemName, itemType, canPlaceInShoulder, gold, silver, copper, isKeyItem, inventoryTextureSpritePrefabName, layerAdd, hit, range, numberOfDamageDice, diceType, damageBonus, damageType, criticalChance, durabilityChance, isRanged, shape);
+		//		return weapon;
+	}
+}
