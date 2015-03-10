c4fbe80853cb729d14ba7d902d3c6afb83012d99
Jsdodgers <jsdodgers@aim.com>
2015-03-09 22:52:16 -0700

    Black Market Sell.

--- BelNix/Assets/Scripts/BlackMarketItemContainer.cs
+++ BelNix/Assets/Scripts/BlackMarketItemContainer.cs
@@ -11,17 +11,24 @@ public class BlackMarketItemContainer : MonoBehaviour {
 	public Text priceText;
 	public Button buyButton;
 	BaseManager bm = null;
+	public bool selling = false;
 
 	public void buyItem() {
 		if (bm != null) {
-			bm.buyItem(item.editorItem.getItem());
+			if (selling) {
+				bm.sellItem(item.item);
+			}
+			else {
+				bm.buyItem(item.editorItem.getItem());
+			}
 		}
 	}
-	public void setUp(BlackMarketItem item, BaseManager bm) {
+	public void setUp(BlackMarketItem item, BaseManager bm, bool selling) {
+		this.selling = selling;
 		this.bm = bm;
 		this.item = item;
 		itemText.text = UnitGUI.getSmallCapsString(item.item.getBlackMarketText(), 12);
-		priceText.text = item.item.getBlackMarketPriceText();
+		priceText.text = (selling ? item.item.getBlackMarketSellPriceText() :item.item.getBlackMarketPriceText());
 		itemImage.sprite = item.item.inventoryTexture;
 		Vector2 size = item.item.getSize() * 32.0f;
 		if (size.x > 90 || size.y > 90) size/=2.0f;
@@ -30,8 +37,9 @@ public class BlackMarketItemContainer : MonoBehaviour {
 	}
 
 	public void setCanAfford() {
+
 		int cost = item.item.copper + item.item.silver * 100 + item.item.gold * 10000;
-		buyButton.interactable = bm.stash.canAfford(cost);
+		buyButton.interactable = selling || bm.stash.canAfford(cost);
 	}
 
 	// Use this for initialization
