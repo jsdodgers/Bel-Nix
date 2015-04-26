93daa1c523531badcfad5d89c1f0fbd96e1fb389
jesteven <jesteven@uci.edu>
2015-04-25 22:26:32 -0700

    Added button click SFX to the End Turn button and the Action buttons.

---
+++ BelNix/Assets/Scripts/ButtonClickSFX.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ButtonClickSFX : MonoBehaviour, IPointerDownHandler, IPointerUpHandler  {
+
+    [SerializeField, Range(0, 1)]
+    private float volume;
+
+    private AudioManager audioManager;
+    private bool currentlyClicking = false;
+
+
+	// Use this for initialization
+	void Start () {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPointerDown(PointerEventData e)
+    {
+        //buttonDown();
+        audioManager.playAudioClip(SFXClip.ButtonDown, volume);
+    }
+    public void OnPointerUp(PointerEventData e)
+    {
+        //buttonUp();
+        audioManager.playAudioClip(SFXClip.ButtonUp, volume);
+    }
+
+
+    public void buttonDown()
+    {
+        if(!currentlyClicking)
+        {
+            audioManager.playAudioClip(SFXClip.ButtonDown, 1.0f);
+            currentlyClicking = true;
+            Debug.Log("Button Down!");
+        }
+    }
+
+    public void buttonUp()
+    {
+        audioManager.playAudioClip(SFXClip.ButtonUp, 1.0f);
+        currentlyClicking = false;
+        Debug.Log("Button Up!");
+    }
+}
