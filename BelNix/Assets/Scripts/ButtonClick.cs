66deacef0786e1792eea5d34bdd7f50b160b3380
jesteven <jesteven@uci.edu>
2015-04-25 19:47:24 -0700

    Testing button clicking sounds on the End Turn button. Added button up and down sound effects to resource folder and audiomanager.

---
+++ BelNix/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClick : MonoBehaviour {
+    AudioManager audioManager;
+    private bool currentlyClicking = false;
+
+	// Use this for initialization
+	void Start () {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void buttonDown()
+    {
+        if(!currentlyClicking)
+        {
+            audioManager.playAudioClip(SFXClip.ButtonDown, 0.8f);
+            currentlyClicking = true;
+            Debug.Log("Button Down!");
+        }
+    }
+
+    public void buttonUp()
+    {
+        audioManager.playAudioClip(SFXClip.ButtonUp, 0.8f);
+        currentlyClicking = false;
+        Debug.Log("Button Up!");
+    }
+}
