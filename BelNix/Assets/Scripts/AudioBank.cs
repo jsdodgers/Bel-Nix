4a6d84c6aea6eb6787367beeaec43e64c4c4c8f4
Jonathan Stevens <jesteven@uci.edu>
2015-05-06 19:27:12 -0700

    Cleaned up a little code in AudioBank. Moved some more audio files.

--- BelNix/Assets/Scripts/AudioBank.cs
+++ BelNix/Assets/Scripts/AudioBank.cs
@@ -18,9 +18,6 @@ public enum SFXClip {
 
 public class AudioBank : MonoBehaviour  {
 
-	
-	//AudioClip crushingSwing;
-	//AudioClip crushingHit;
     Dictionary<SFXClip, AudioClip> clipList;
     Queue<GameObject> SFXPlayers;
     GameObject SFXContainerTemplate;
@@ -28,10 +25,6 @@ public class AudioBank : MonoBehaviour  {
 
 	// Use this for initialization
 	void Start ()  {
-		//crushingSwing = Resources.Load<AudioClip>("Audio/SFX/Combat-CrushSwing");
-		//crushingHit = Resources.Load<AudioClip>("Audio/SFX/Combat-CrushHit");
-	//	crushingHit = Resources.Load<AudioClip>("Audio/SFX/Scratchy Record");
-
         clipList = new Dictionary<SFXClip, AudioClip>();
         SFXPlayers = new Queue<GameObject>();
         SFXContainerTemplate = new GameObject("SFX Player", typeof(AudioSource));
@@ -42,20 +35,20 @@ public class AudioBank : MonoBehaviour  {
 
     private void loadSFX()
     {
-        importAudioClip(SFXClip.Footstep1, "footstep1");
-        importAudioClip(SFXClip.Footstep2, "footstep2");
-        importAudioClip(SFXClip.Footstep3, "footstep3");
-        importAudioClip(SFXClip.Footstep4, "footstep4");
-        importAudioClip(SFXClip.TurretShoot, "turret-shoot");
-        importAudioClip(SFXClip.UISpark, "zapv1");
-        importAudioClip(SFXClip.BloodSplash, "blood-splash");
-        importAudioClip(SFXClip.BluntImpact, "Combat-CrushHit");
-        importAudioClip(SFXClip.CrushingSwing, "Combat-CrushSwing");
-        importAudioClip(SFXClip.CrushingHit, "Combat-CrushHit");
-        importAudioClip(SFXClip.ComposureDamage, "composure-hit");
+        importAudioClip(SFXClip.Footstep1,      "footstep1");
+        importAudioClip(SFXClip.Footstep2,      "footstep2");
+        importAudioClip(SFXClip.Footstep3,      "footstep3");
+        importAudioClip(SFXClip.Footstep4,      "footstep4");
+        importAudioClip(SFXClip.TurretShoot,    "turret-shoot");
+        importAudioClip(SFXClip.UISpark,        "zapv1");
+        importAudioClip(SFXClip.BloodSplash,    "blood-splash");
+        importAudioClip(SFXClip.BluntImpact,    "Combat-CrushHit");
+        importAudioClip(SFXClip.CrushingSwing,  "Combat-CrushSwing");
+        importAudioClip(SFXClip.CrushingHit,    "Combat-CrushHit");
+        importAudioClip(SFXClip.ComposureDamage,"composure-hit");
         importAudioClip(SFXClip.ComposureBreak, "composure-break");
-        importAudioClip(SFXClip.ButtonDown, "Button_DOWN");
-        importAudioClip(SFXClip.ButtonUp, "Button_UP");
+        importAudioClip(SFXClip.ButtonDown,     "Button_DOWN");
+        importAudioClip(SFXClip.ButtonUp,       "Button_UP");
     }
 
     public void importAudioClip(SFXClip key, string filename)
@@ -73,28 +66,11 @@ public class AudioBank : MonoBehaviour  {
         clipList.TryGetValue(clipName, out clip);
         if (clip == null)
             return;
-		//float volume = 1.0f;
 		AudioSource.PlayClipAtPoint(clip, position, volume);
 	}
 
     public void playAudioClip(SFXClip clipName, float volume = 1.0f)
     {
-        //AudioClip clip;
-        //clipList.TryGetValue(clipName, out clip);
-        //if (clip == null)
-        //    return;
-
-        //GameObject newSFXPlayer = (GameObject)Instantiate(SFXContainerTemplate);
-        //newSFXPlayer.transform.SetParent(transform, false);
-
-        //if (SFXPlayers.Count >= MAX_SFXPLAYER_COUNT)
-        //    Destroy(SFXPlayers.Dequeue());
-        //SFXPlayers.Enqueue(newSFXPlayer);
-
-        //AudioSource SFXPlayer = newSFXPlayer.GetComponent<AudioSource>();
-        //SFXPlayer.clip = clip;
-        //SFXPlayer.volume = volume;
-        //SFXPlayer.Play();
         playClipAtPoint(clipName, Camera.main.transform.position, volume);
     }
 }
