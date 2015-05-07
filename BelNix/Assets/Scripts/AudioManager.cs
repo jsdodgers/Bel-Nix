39a998878c36b2da8c86abcfcac28a1bd34d0b45
Jonathan Stevens <jesteven@uci.edu>
2015-05-06 19:15:06 -0700

    Changed the logic behind playing sound effects. Moved my SFX stuff into AudioBank. Changed the method signatures for playClipAtPoint (uses SFXClip instead of ClipName now) Fixed a null reference exception in MapGenerator's fadeInMusic() method (the audioManager was null for some reason, so I added a check and correction for that)

--- BelNix/Assets/Scripts/AudioManager.cs
+++ BelNix/Assets/Scripts/AudioManager.cs
@@ -1,8 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-
-public enum SFXClip {UISpark, TurretShoot, Footstep1, Footstep2, Footstep3, Footstep4, BloodSplash, BluntImpact, ComposureDamage, ComposureBreak, ButtonUp, ButtonDown}
 
 public class AudioManager : MonoBehaviour  {
 	public GameObject phazingMusic;
@@ -11,16 +8,13 @@ public class AudioManager : MonoBehaviour  {
 	AudioSource cMusic;
 	Animator anim;
 	CleanMusicLoop cml;
-    Dictionary<SFXClip, AudioClip> clipList;
-    Queue<GameObject> SFXPlayers;
-    GameObject SFXContainerTemplate;
-    private const int MAX_SFXPLAYER_COUNT = 20;
+
+    AudioBank audioBank;
     private static AudioManager primaryAudioManager;
     
 	[SerializeField] private float transitionTime;
 	// Use this for initialization
 	void Start ()  {
-        primaryAudioManager = this;
 		if (phazingMusic != null)  {
 			music = phazingMusic.GetComponent<AudioSource>();
 			anim = phazingMusic.GetComponent<Animator>();
@@ -29,52 +23,18 @@ public class AudioManager : MonoBehaviour  {
 		if (constantMusic != null)  {
 			cMusic = constantMusic.GetComponent<AudioSource>();
 		}
-        clipList = new Dictionary<SFXClip, AudioClip>();
-        SFXPlayers = new Queue<GameObject>();
-        SFXContainerTemplate = new GameObject("SFX Player", typeof(AudioSource));
-        SFXContainerTemplate.transform.SetParent(transform);
-        SFXContainerTemplate.GetComponent<AudioSource>().playOnAwake = false;
-        loadSFX();
+        primaryAudioManager = this;
+        audioBank = MapGenerator.mg.audioBank;
 	}
 
-    private void loadSFX()
+    public void playAudioClip(SFXClip clipName, float volume = 1.0f)
     {
-        importAudioClip(SFXClip.Footstep1, "footstep1");
-        importAudioClip(SFXClip.Footstep2, "footstep2");
-        importAudioClip(SFXClip.Footstep3, "footstep3");
-        importAudioClip(SFXClip.Footstep4, "footstep4");
-        importAudioClip(SFXClip.TurretShoot, "turret-shoot");
-        importAudioClip(SFXClip.UISpark, "zapv1");
-        importAudioClip(SFXClip.BloodSplash, "blood-splash");
-        importAudioClip(SFXClip.BluntImpact, "Combat-CrushHit");
-        importAudioClip(SFXClip.ComposureDamage, "composure-hit");
-        importAudioClip(SFXClip.ComposureBreak, "composure-break");
-        importAudioClip(SFXClip.ButtonDown, "Button_DOWN");
-        importAudioClip(SFXClip.ButtonUp, "Button_UP");
-    }
-    public void importAudioClip(SFXClip key, string filename)
-    {
-        clipList.Add(key, Resources.Load<AudioClip>("Audio/SFX/" + filename));
+        audioBank.playAudioClip(clipName, volume);
     }
 
-    public void playAudioClip(SFXClip clipName, float volume)
+    public void playAudioClip(SFXClip clipName, Vector3 position, float volume = 1.0f)
     {
-        AudioClip clip;
-        clipList.TryGetValue(clipName, out clip);
-        if (clip == null)
-            return;
-
-        GameObject newSFXPlayer = (GameObject) Instantiate(SFXContainerTemplate);
-        newSFXPlayer.transform.SetParent(transform, false);
-
-        if (SFXPlayers.Count >= MAX_SFXPLAYER_COUNT)
-            Destroy(SFXPlayers.Dequeue());
-        SFXPlayers.Enqueue(newSFXPlayer);
-
-        AudioSource SFXPlayer = newSFXPlayer.GetComponent<AudioSource>();
-        SFXPlayer.clip = clip;
-        SFXPlayer.volume = volume;
-        SFXPlayer.Play();
+        audioBank.playClipAtPoint(clipName, position, volume);
     }
 
     public static AudioManager getAudioManager()
